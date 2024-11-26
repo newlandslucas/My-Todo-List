01646ac8deb72dbcabe7c19b2b5a56df446657c5
newlandslucas <lucasnewlands.m@gmail.com>
2024-11-26 16:06:01 -0300

    Initial Commit

---
+++ MyTodoList/MyTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTodoListApp.swift
+//  MyTodoList
+//
+//  Created by Lucas Newlands on 26/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MyTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
